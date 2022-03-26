dd3f74957d7245abac5649085dd00571c65a50e9
AngelIzaguirre <angel.izaguirre@ujcv.edu.hn>
2022-03-25 18:23:28 -0600

    codigo completo

---
+++ PI_2022_I_L2_EQUIPO2/Objetos/ComunListBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_2022_I_L2_EQUIPO2.Objetos
+{
+    internal abstract class ComunListBase
+    {
+        public ComunListBase()
+        {
+        }
+        public void Agregar()
+        {
+            
+        }
+        public void Eliminar()
+        {
+            
+        }
+
+    }
+}
